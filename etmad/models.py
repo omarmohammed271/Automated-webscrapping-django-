d812f57742321281f6be6295e9b6b6af3eb81d07
moooooora <omarmohammed271@gmail.com>
2024-09-24 09:54:26 +0300

    Design Model & Make Management command for Scrapp the data

--- etmad/models.py
+++ etmad/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Tender(models.Model):
+
+    ref_number = models.CharField( max_length=70)
+    title = models.CharField( max_length=100)
+    price = models.CharField( max_length=50)
+    date = models.DateField()
+    dateline = models.DateField()
+
+    class Meta:
+        verbose_name = ("Tender")
+        verbose_name_plural = ("Tenders")
+
+    def __str__(self):
+        return self.title
+
+    
